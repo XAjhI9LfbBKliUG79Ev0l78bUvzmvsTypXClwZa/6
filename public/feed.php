e6b02802b3e54816870fa62d9871cf4da39cd8ef
XAjhI9LfbBKliUG79Ev0l78bUvzmvsTypXClwZa <229113911+XAjhI9LfbBKliUG79Ev0l78bUvzmvsTypXClwZa@users.noreply.github.com>
2025-11-18 11:34:40 +0400

    Update feed.php

--- public/feed.php
+++ public/feed.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../src/init.php';
-require_once __DIR__ . '/../src/news.php';
+require_once __DIR__ . '/../public/news.php';
 
 if (!isset($_SESSION['user'])) {
     header('Location: index.php');
